e582e7b0aad8c609f8e7328e20179120bcc823a6
Mou7amed0 <outloohou@outlook.com>
2022-02-16 21:49:04 +0100

    Added migrations

---
+++ database/migrations/2022_02_16_163900_create_etudiants_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return voidph
+     */
+    public function up()
+    {
+        Schema::create('etudiants', function (Blueprint $table) {
+            $table->id();
+            $table->string("apogee");
+            $table->string('cne')->unique();
+            $table->string('cin')->unique();
+            $table->string('nom');
+            $table->string('prenom');
+            $table->date("date_de_naissance");
+            $table->string("adresse");
+            $table->string('email_institutionnel');
+            $table->string('email_personnel')->nullable();
+            $table->enum("sexe", ["male", "female"]);
+
+            //foreign key
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on("users");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('etudiants');
+    }
+};
